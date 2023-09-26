f57a2a2c4bf61e6a92ccb86fbaa51f1fb26416f4
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-26 15:21:43 +0300

    Update exercise09.py

--- module02-data.structures/exercise09.py
+++ module02-data.structures/exercise09.py
@@ -2,3 +2,8 @@ area_codes = {"izmir": 232, "ankara": 312, "istanbul": {"anadolu": 216, "avrupa"
 print("Area code for 'Ankara' is", area_codes['ankara'])
 print(f"Area code for 'Ankara' is {area_codes['ankara']}")
 print("Area code for %s is %s" % ('Ankara', area_codes['ankara']))
+print("Area code for %s is %s" % ('istanbul-anadolu', area_codes['istanbul']['anadolu']))
+print("Area code for %s is %s" % ('istanbul-avrupa', area_codes['istanbul']['avrupa']))
+area_codes["antalya"] = 242
+print(area_codes.keys())
+print(area_codes.values())
