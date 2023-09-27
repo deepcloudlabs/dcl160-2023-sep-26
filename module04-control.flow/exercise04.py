3b259460fd419f2590aa73eb9f680b632dd19dde
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-27 11:37:52 +0300

    Update exercise04.py

--- module04-control.flow/exercise04.py
+++ module04-control.flow/exercise04.py
@@ -9,4 +9,16 @@ Leap Year Test
 2200 Not a Leap Year
 2300 Not a Leap Year
 2400 Leap Year
+11:20
 """
+year = int(input("enter an integer: "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
