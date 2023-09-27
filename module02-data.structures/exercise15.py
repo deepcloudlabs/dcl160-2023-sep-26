3f9a35b9981bc933fe5f73e7e547227018f20be2
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-27 07:13:30 +0300

    Update exercise15.py

--- module02-data.structures/exercise15.py
+++ module02-data.structures/exercise15.py
@@ -1,9 +1,14 @@
 employees = [
     {"fullname": "jack shephard", "department": "Sales", "salary": 100000, "year": 1978, "fulltime": True},
     {"fullname": "kate austen", "department": "IT", "salary": 300000, "year": 1985, "fulltime": False},
-    {"fullname": "ben linus", "department": "Finance", "salary": 150000, "year": 1967, "fulltime": True},
+    {"fullname": "ben linus", "department": "Finance", "salary": 120000, "year": 1967, "fulltime": True},
     {"fullname": "james sawyer", "department": "HR", "salary": 70000, "year": 1979, "fulltime": True},
     {"fullname": "kim kwon", "department": "Sales", "salary": 120000, "year": 1986, "fulltime": True},
     {"fullname": "sun kwon", "department": "IT", "salary": 200000, "year": 1984, "fulltime": False},
     {"fullname": "hugo reyes", "department": "IT", "salary": 120000, "year": 1992, "fulltime": True}
 ]
+
+print(employees[0]["salary"])
+print(employees[-1]["salary"])
+employees.sort(key=lambda emp: (emp["department"], emp["fullname"]))
+print(employees)
