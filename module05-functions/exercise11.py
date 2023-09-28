7d51569a5fac1f9799b92fe0136d2f2c1c725513
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-28 10:46:04 +0300

    Create exercise11.py

---
+++ module05-functions/exercise11.py
@@ -0,0 +1,7 @@
+# 3716842
+digits = [3, 7, 1, 6, 8, 4, 2]
+print(digits)
+digits_as_str = [str(digit) for digit in digits]
+print(digits_as_str)
+print("".join(['1', '2', '3', '4', '5', '6', '7', '8', '9']))
+print(int("".join([str(digit) for digit in digits])))
