e8ed69a3277b29999beef032b583d3ad8a16771b
Binnur KURT <deepcloudlabs@gmail.com>
2023-09-29 11:11:00 +0300

    Update exercise02.py

--- module08-oop.in.python/exercise02.py
+++ module08-oop.in.python/exercise02.py
@@ -10,6 +10,12 @@ class AccountStatus(Enum):
     BLOCKED = 300
 
 
+class InsufficientBalanceError(Exception):
+    def __init__(self, message: str, deficit: float):
+        self.message = message
+        self.deficit = deficit
+
+
 class Account:
     """
     Account is a class -> abstraction
@@ -25,10 +31,69 @@ class Account:
         :param balance: the amount saved in the account
         :param status: the status of the account, e.g. ACTIVE, CLOSED, etc.
         """
-        self.iban = iban
-        self.balance = balance
-        self.status = status
+        self._iban = iban
+        self._balance = balance
+        self._status = status
+
+    @property
+    def iban(self):
+        return self._iban
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, new_status):
+        if not self._status == AccountStatus.CLOSED:
+            self._status = new_status
+
+    def deposit(self, amount: float) -> float:
+        """
+        deposit some amount to the balance
+        :param amount: amount to deposit. amount should be a positive float. if it is less than or equal
+           to zero the method raises ValueError
+        :returns the balance after deposit
+        """
+        # validation rule
+        if amount <= 0.0:
+            raise ValueError("You must provide a positive amount to deposit")
+        # policy
+        if not self._status == AccountStatus.ACTIVE:
+            raise ValueError(f"Account is not ACTIVE to deposit: {self._status.name}")
+        self._balance += amount
+        return self._balance
+
+    def withdraw(self, amount: float) -> float:
+        # validation rule
+        if amount <= 0.0:
+            raise ValueError("You must provide a positive amount to withdraw")
+        # policy
+        if not self._status == AccountStatus.ACTIVE:
+            raise ValueError(f"Account is not ACTIVE to withdraw: {self._status.name}")
+        # business rule
+        if amount > self._balance:
+            deficit = amount - self._balance
+            raise InsufficientBalanceError("Your balance does not cover your expenses", deficit)
+        self._balance -= amount
+        return self._balance
 
 
 acc1 = Account("tr1", 1_000_000)  # object
+acc2 = Account("tr2", 2_000_000)  # object
 print(f"balance in acc1 is {acc1.balance}")
+acc1.withdraw(250_000) # withdraw(acc1, 250_000)
+acc2.withdraw(250_000) # withdraw(acc2, 250_000)
+acc2.deposit(375_000) # deposit(acc2, 250_000)
+print(f"balance in acc2 is {acc2._balance}")
+acc2.status = AccountStatus.BLOCKED
+print(f"status of acc2 is {acc2.status.name}")
+acc2.status = AccountStatus.CLOSED
+print(f"status of acc2 is {acc2.status.name}")
+acc2.status = AccountStatus.ACTIVE
+print(f"status of acc2 is {acc2.status.name}")
+
